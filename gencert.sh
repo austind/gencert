a2895285d0cc178580810e3cd034b88eebd33af0
Austin de Coup-Crank <austindcc@gmail.com>
2020-08-10 11:15:45 -0700

    Functions 'n' stuff

--- gencert.sh
+++ gencert.sh
@@ -3,16 +3,20 @@
 # No trailing slash
 OUTPREFIX="./out"
 
-# These values are required for the CSR
+# These values are required to create the CSR
 # but overwritten by the CA in most cases
 CSR_C="US"
 CSR_ST="Being"
 CSR_L="Springfield"
 CSR_O="Dis"
 
+DIV="***************************************************"
+
 echo "Domain you want a certificate for. Wildcard certs: use \"*.example.org\""
 read -rp "Domain: " DOMAIN
+echo ""
 
+# Asterisks in paths are allowed, but make things harder
 if [[ "${DOMAIN}" == *'*'* ]]; then
     SLD=$(echo "${DOMAIN}" | sed 's/\*\.//g')
     NAME="${SLD}-wildcard"
@@ -21,21 +25,69 @@ else
 fi
 
 OUTPATH="${OUTPREFIX}/${NAME}"
-OUTNAME="${OUTPATH}/${NAME}"
 
 if [ ! -d "${OUTPATH}" ]; then
     mkdir "${OUTPATH}"
 fi
 
-KEY="${OUTNAME}.key"
-CSR="${OUTNAME}.csr"
+KEY="${NAME}.key"
+CSR="${NAME}.csr"
+CRT="${NAME}.crt"
+PFX="${NAME}.pfx"
+KEYPATH="${OUTPATH}/${KEY}"
+CSRPATH="${OUTPATH}/${CSR}"
+CRTPATH="${OUTPATH}/${CRT}"
+PFXPATH="${OUTPATH}/${PFX}"
 
-# If key exists without CSR
-if [[ -f "${KEY}" ]] && [[ ! -f "$CSR" ]]; then
-    # Ask to rekey or generate CSR based on key
-    :
+new_csr_from_key() {
+    openssl req -new -sha256 -key ${KEYPATH} -out ${CSRPATH} \
+        -subj "/C=${CSR_C}/ST=${CSR_ST}/L=${CSR_L}/O=${CSR_O}/CN=${DOMAIN}"
+}
+
+new_csr_and_key() {
+    openssl req -new -sha256 -newkey rsa:4096 -nodes \
+        -keyout ${KEYPATH} -out ${CSRPATH} \
+        -subj "/C=${CSR_C}/ST=${CSR_ST}/L=${CSR_L}/O=${CSR_O}/CN=${DOMAIN}"
+}
+
+csr_instructions() {
+    echo ""
+    echo "${DIV}"
+    echo " 1. Submit CSR to CA"
+    echo " 2. Place signed ${CRT} in ${OUTPATH}"
+    echo " 3. Re-run gencert.sh with Domain: ${DOMAIN}"
+    echo "${DIV}"
+    echo ""
+}
+
+new_pfx() {
+    openssl pkcs12 -export -out ${PFXPATH} \
+        -inkey ${KEYPATH} -in ${CRTPATH}
+}
+
+# If key and CSR exist
+if [[ -f "${KEYPATH}" ]] && [[ -f "${CSRPATH}" ]]; then
+    # Check for signed cert
+    if [[ -f "${CRTPATH}" ]]; then
+        # Check for PFX
+        if [[ -f "${PFXPATH}" ]]; then
+            echo "Found PFX: ${PFXPATH}"
+            echo "Nothing to do."
+        else
+            new_pfx
+        fi
+   else
+        echo "Error: no signed cert found in ${OUTPATH}"
+        csr_instructions
+        # TODO: Do you want to re-generate CSR and/or key?
+    fi
+# Key exists without CSR
+elif [[ -f "${KEYPATH}" ]] && [[ ! -f "${CSRPATH}" ]]; then
+    echo "Generating new CSR from existing key: ${KEYPATH}"
+    new_csr_from_key
+    csr_instructions
+else
+    echo "Generating new key and CSR"
+    new_csr_and_key
+    csr_instructions
 fi
-
-openssl req -new -sha256 -newkey rsa:4096 -nodes \
-    -keyout ${OUTNAME}.key -out ${OUTNAME}.csr \
-    -subj "/C=${CSR_C}/ST=${CSR_ST}/L=${CSR_L}/O=${CSR_O}/CN=${DOMAIN}"
