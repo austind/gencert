75a1aa8ecba1f8b1fc731155bb133909d3fa9218
Austin de Coup-Crank <austindcc@gmail.com>
2020-08-10 10:26:01 -0700

    improvements

--- gencert.sh
+++ gencert.sh
@@ -1,12 +1,36 @@
 #!/bin/bash
 
+# No trailing slash
+OUTPREFIX="./out"
+
+# These values are required for the CSR
+# but overwritten by the CA in most cases
+CSR_C="US"
+CSR_ST="Being"
+CSR_L="Springfield"
+CSR_O="Dis"
+
+echo "Domain you want a certificate for. Wildcard certs: use \"*.example.org\""
 read -rp "Domain: " DOMAIN
-if [ ! -d "./${DOMAIN}" ]; then
-    mkdir "./${DOMAIN}"
+
+if [[ "${DOMAIN}" == *'*'* ]]; then
+    SLD=$(echo "${DOMAIN}" | sed 's/\*\.//g')
+    echo $SLD
+    NAME="${SLD}-wildcard"
+    echo $NAME
+else
+    NAME="${SLD}"
 fi
-OUTPUT="./${DOMAIN}/${DOMAIN}"
-echo "Generating new private key..."
-openssl genrsa -out "${OUTPUT}.key" 2048
-echo "Generating CSR for ${OUTPUT}"
-openssl req -new -sha256 -key "${OUTPUT}.key" -out "${OUTPUT}.csr"
+
+OUTPATH="${OUTPREFIX}/${NAME}"
+OUTNAME="${OUTPATH}/${NAME}"
+
+if [ ! -d "${OUTPATH}" ]; then
+    mkdir "${OUTPATH}"
+fi
+
+echo "Generating key & CSR for ${DOMAIN}"
+openssl req -new -sha256 -newkey rsa:4096 -nodes \
+    -keyout ${OUTNAME}.key -out ${OUTNAME}.csr \
+    -subj "/C=${CSR_C}/ST=${CSR_ST}/L=${CSR_L}/O=${CSR_O}/CN=${DOMAIN}"
 
